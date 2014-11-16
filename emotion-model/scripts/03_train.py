38eba5237701f0b1c45ddabb5a587e93af06c30d
Xinyu Zhou <zxytim@gmail.com>
2014-11-16 13:14:33 +0800

    training script

--- emotion-model/scripts/03_train.py
+++ emotion-model/scripts/03_train.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: 03_train.py
-# $Date: Sat Nov 15 17:25:01 2014 +0800
+# $Date: Sun Nov 16 00:24:35 2014 +0800
 # $Author: Xinyu Zhou <zxytim[at]gmail[dot]com>
 
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import *
+from sklearn.ensemble import *
 from sklearn import metrics
 from lmr.utils import serial
 from lmr.learn import split_data
@@ -12,10 +14,16 @@ from lmr.learn import split_data
 import argparse
 
 _MODEL_DICT = dict(
-    lr=LinearRegression)
+    lr=LinearRegression,
+    etr=ExtraTreeRegressor,
+    rfr=RandomForestRegressor,
+    gbr=GradientBoostingRegressor
+)
 
 def get_model(name, *args, **kwargs):
-    assert name in _MODEL_DICT, 'unrecognized model name: `{}\''.format(name)
+    assert name in _MODEL_DICT, (
+        'unrecognized model name: `{}\'. available names are: {}'.format(
+            name, ', '.join(_MODEL_DICT)))
     return _MODEL_DICT[name](*args, **kwargs)
 
 
@@ -31,21 +39,27 @@ def main():
     parser.add_argument('-r', '--train-ratio', default=0.8, type=float)
     parser.add_argument('-m', '--model-name', required=True,
                         help='sklearn model names')
+    parser.add_argument('-a', '--all-train', action='store_true')
     parser.add_argument('-o', '--model-output', required=True)
     args = parser.parse_args()
 
-    X, y = serial.load(args.pickled_data)
-    X_train, y_train, X_val, y_val = split_data(X, y, args.train_ratio)
-
     mdl = get_model(args.model_name)
-    mdl.fit(X_train, y_train)
 
-    serial.dump(mdl, args.model_output)
+    X, y = serial.load(args.pickled_data)
+    if args.all_train:
+        mdl.fit(X, y)
+        serial.dump(mdl, args.model_output)
+        print 'on training set:'
+        evaluate(mdl, X, y)
+    else:
+        X_train, y_train, X_val, y_val = split_data(X, y, args.train_ratio)
+        mdl.fit(X_train, y_train)
+        serial.dump(mdl, args.model_output)
+        print 'on training set:'
+        evaluate(mdl, X_train, y_train)
+        print 'on evaluation set:'
+        evaluate(mdl, X_val, y_val)
 
-    print 'on training set:'
-    evaluate(mdl, X_train, y_train)
-    print 'on evaluation set:'
-    evaluate(mdl, X_val, y_val)
 
 
 
